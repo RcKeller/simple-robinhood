27c077a60127f8cba216e76b38245267522f64ba
Andy Keller <andy@barrfitz.com>
2023-10-09 12:55:42 -0700

    feat: update code to use new env var names
    fix: update gitignore

--- simplehood_api/controllers/tickers.controller.js
+++ simplehood_api/controllers/tickers.controller.js
@@ -1,25 +1,25 @@
-const http = require("http");
+const http = require('http');
 
 // API Route to fetch list of tickers
 const getTickers = (req, res) => {
   // Options for request
   const options = {
-    hostname: process.env.API_URL || "localhost",
-    port: process.env.API_PORT || 5000,
-    path: "/tickers",
-    method: "GET",
+    hostname: process.env.API_URL || 'localhost',
+    port: process.env.SIMDAQ_PORT || 5000,
+    path: '/tickers',
+    method: 'GET',
   };
   const http_req = http.request(options, (http_res) => {
-    var body = "";
-    http_res.on("data", (d) => {
+    var body = '';
+    http_res.on('data', (d) => {
       body += d;
     });
-    http_res.on("end", () => {
+    http_res.on('end', () => {
       // Request complete. Return data
       res.json(JSON.parse(body));
     });
   });
-  http_req.on("error", (error) => {
+  http_req.on('error', (error) => {
     console.error(error);
   });
   http_req.end();
