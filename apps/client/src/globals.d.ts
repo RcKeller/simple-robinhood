19be34e49aa3412940535ecd993694dcea914b95
Andy Keller <andy@barrfitz.com>
2023-10-09 23:11:11 -0700

    fix: plug an issue w/ missing ts outputs

--- apps/client/src/globals.d.ts
+++ apps/client/src/globals.d.ts
@@ -3,3 +3,5 @@ declare module '*.svg';
 declare module '*.jpeg';
 declare module '*.jpg';
 declare module '*.module.css';
+// TODO: fix build issue with missing d.ts in output
+declare module '@rckeller/robinhood-ui';
